6d7762fdb8ae3ed9714e4978b7efc1d3813c8767
Diego Villamil <dmvillamil@up.edu.ph>
2019-08-25 22:49:38 +0800

    ADDSTUDENT now works but VIEWSTUDENT doesn't nagpirpint lang yung mga enter ____ stuff

--- Villamil_01.cbl
+++ Villamil_01.cbl
@@ -14,10 +14,11 @@
                  04 landline PIC X(8).
                03 age PIC 99.
            77 EXITED PIC 9 VALUE 0.
-           77 IT PIC 99.
+           77 IT PIC 99 VALUE 1.
            77 CHOICE PIC 9.
            77 NUMstd PIC 9 VALUE 0.
-           77 STDchoice PIC 9 VALUE 0.
+           77 STDchoice PIC 9 VALUE 1.
+           77 STDsearch PIC X(30).
 
        PROCEDURE DIVISION.
 
@@ -36,6 +37,7 @@
 
              IF CHOICE = 1 THEN
                DISPLAY "PREPARING TO ADD STUDENT...."
+               PERFORM ADDSTUDENT
              ELSE
                IF CHOICE = 2 THEN
                  DISPLAY "E"
@@ -45,6 +47,7 @@
                  ELSE
                    IF CHOICE = 4 THEN
                      DISPLAY "C"
+                     PERFORM VIEWSTUDENT
                    ELSE
                      IF CHOICE = 5 THEN
                        DISPLAY "H"
@@ -67,12 +70,19 @@
           ELSE
             DISPLAY "Student slot " WITH NO ADVANCING
             DISPLAY IT WITH NO ADVANCING
-            DISPLAY "is occupied"
+            DISPLAY " is occupied"
+          END-IF.
+         
+         FINDSTD.
+          IF STDsearch IS NOT EQUAL TO sNo(IT) THEN
+            COMPUTE IT = IT + 1
           END-IF.
 
          ADDSTUDENT.
            IF NUMstd < 5 THEN
+             MOVE 1 TO IT
              PERFORM CHOOSESTD
+             DISPLAY IT
              DISPLAY "Enter student number: " WITH NO ADVANCING
              ACCEPT sNo(IT)
              DISPLAY "Enter student full name: " WITH NO ADVANCING
@@ -85,9 +95,31 @@
              ACCEPT landline(IT)
              DISPLAY "Enter student age: " WITH NO ADVANCING
              ACCEPT age(IT)
+             COMPUTE NUMstd = NUMstd + 1
+             MOVE 1 TO IT
            ELSE
              DISPLAY "The student directory is full"
            END-IF.
+
          VIEWSTUDENT.
-           
+           IF NUMstd > 0 THEN
+             DISPLAY "Enter his/her student number: " WITH NO ADVANCING
+             DISPLAY STDsearch
+             PERFORM FINDSTD UNTIL STDsearch IS EQUAL TO sNo(IT)
+             DISPLAY "student number: " WITH NO ADVANCING
+             DISPLAY sNo(IT)
+             DISPLAY "student full name: " WITH NO ADVANCING
+             DISPLAY fullname(IT)
+             DISPLAY "student course: " WITH NO ADVANCING
+             DISPLAY course(IT)
+             DISPLAY "student mobile no. : " WITH NO ADVANCING
+             DISPLAY mobile(IT)
+             DISPLAY "student landline no. : " WITH NO ADVANCING
+             DISPLAY landline(IT)
+             DISPLAY "student age: " WITH NO ADVANCING
+             DISPLAY age(IT)
+             MOVE 1 TO IT
+           ELSE
+             DISPLAY "There are no students to view in the directory"
+           END-IF.
         END PROGRAM.
